7bc4ba5bac7cec32f0c6b4a8d621e3cc3a1fd92f
Tomaz Berisa <tomaz.berisa@gmail.com>
2015-03-24 17:00:14 -0400

    Added dependency on Py 3

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+import sys
 from distutils.core import setup
+
+if sys.version_info<(3,):
+	raise Exception('Sorry, only Python v3 and higher are supported')
+
 setup(
   name = 'commanderline',
   packages = ['commanderline'], 
-  version = '0.1.4',
+  version = '0.1.5',
   description = 'Easily expose any function to the command line',
   author = 'Tomaz Berisa',
   author_email = 'tomaz.berisa@gmail.com',
   url = 'https://bitbucket.org/tomazberisa/commanderline', 
-  download_url = 'https://bitbucket.org/tomazberisa/commanderline/get/0.1.4.tar.gz', 
-  keywords = ['command-line'], 
+  download_url = 'https://bitbucket.org/tomazberisa/commanderline/get/0.1.5.tar.gz', 
+  keywords = ['command-line', 'command line', 'cli'], 
   classifiers = [],
 )
