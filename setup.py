ba881db79a8018e9cf328e5225a3d90ff7ccff6e
Tomaz Berisa <tomaz.berisa@gmail.com>
2015-03-24 14:49:55 -0400

    final 0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'commanderline',
   packages = ['commanderline'], 
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Easily expose any function to the command line',
   author = 'Tomaz Berisa',
   author_email = 'tomaz.berisa@gmail.com',
   url = 'https://bitbucket.org/tomazberisa/commanderline', 
-  download_url = 'https://bitbucket.org/tomazberisa/commanderline/get/0.1.2.tar.gz', 
+  download_url = 'https://bitbucket.org/tomazberisa/commanderline/get/0.1.3.tar.gz', 
   keywords = ['command-line'], 
   classifiers = [],
 )
