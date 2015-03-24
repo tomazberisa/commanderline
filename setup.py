8162b6f144393086233e0157fdb803acc1a6e3bb
Tomaz Berisa <tomaz.berisa@gmail.com>
2015-03-24 12:58:42 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'commanderline',
+  packages = ['commanderline'], 
+  version = '0.1',
+  description = 'Easily expose any function to the command line',
+  author = 'Tomaz Berisa',
+  author_email = 'tomaz.berisa@gmail.com',
+  url = 'https://bitbucket.org/tomazberisa/commanderline', 
+  download_url = 'https://bitbucket.org/tomazberisa/commanderline/tarball/0.1', 
+  keywords = ['command-line'], 
+  classifiers = [],
+)
